79e3536d81782e4d50a8756d916a700dd8393ba7
michmazbout <eliasalalam@outlook.com>
2025-05-04 01:57:02 +0200

    module 1 completed

---
+++ hello_name.py
@@ -0,0 +1,11 @@
+name = "Sam"
+name2 = "Dean"
+ 
+print("Hello "+name)
+print("Hello "+name2)
+
+# An other way is 
+
+name = ["Sam","Dean"]
+for i in name:
+    print("Hello "+i)
--- test_print.py
+++ test_print.py
@@ -4,3 +4,21 @@ print("Have a nice day")
 
 for i in range(5):
   print("This is fun!")
+
+
+print("Programming in Python is fun!")
+
+ratio = 9**(1/2)
+print(ratio)
+
+print(12/(1+2)+2**2)
+
+print(60*60)
+print(26**3)
+
+total_computers = 30*20
+replacement_cycle = 5
+computers_per_year = total_computers/replacement_cycle
+
+print(computers_per_year) # Should print 120.0
+
